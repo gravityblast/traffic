0b4647b95adb231de8863da5e968a892eca65111
Andrea Franz <andrea@gravityblast.com>
2013-08-13 17:08:35 +0100

    more examples on BeforeFilter and NotFoundHandler

--- examples/main.go
+++ examples/main.go
@@ -6,20 +6,31 @@ import (
   "fmt"
 )
 
-func rootHandler(r http.ResponseWriter, req *http.Request) {
-  fmt.Fprint(r, "Hello World")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "Hello World\n")
 }
 
-func pageHandler(r http.ResponseWriter, req *http.Request) {
-  params := req.URL.Query()
-  fmt.Fprintf(r, "Category ID: %s\n", params.Get("category_id"))
-  fmt.Fprintf(r, "Page ID: %s\n", params.Get("id"))
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+  params := r.URL.Query()
+  fmt.Fprintf(w, "Category ID: %s\n", params.Get("category_id"))
+  fmt.Fprintf(w, "Page ID: %s\n", params.Get("id"))
+}
+
+func customNotFoundHandler(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusNotFound)
+  fmt.Fprintf(w, "Page not found: %s\n", r.URL.Path)
+}
+
+func customBeforeFilter(w http.ResponseWriter, r *http.Request) {
+  w.Header().Add("X-APP-NAME", "My App")
 }
 
 func main() {
   router := traffic.New()
   router.Get("/", rootHandler)
   router.Get("/categories/:category_id/pages/:id", pageHandler)
+  router.NotFoundHandler = customNotFoundHandler
+  router.BeforeFilter = customBeforeFilter
 
   http.Handle("/", router)
   http.ListenAndServe(":7000", nil)
