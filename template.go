f5f9f1f334bfbc0255198accf98c56347d24da69
Andrea Franz <andrea@gravityblast.com>
2013-09-18 14:55:11 +0100

    better errors displaying on template parse error

--- template.go
+++ template.go
@@ -32,7 +32,7 @@ func (t *TemplateManager) getTemplatesPaths() []string {
   views := make([]string, 0)
   filepath.Walk(t.templatesPath(), func(path string, info os.FileInfo, err error) (e error) {
     if err != nil {
-      t.templatesParseError = err
+      t.templatesReadError = err
       t.renderFunc = t.RenderTemplateErrors
     } else {
       fileInfo, err := os.Stat(path)
@@ -63,12 +63,12 @@ func (t *TemplateManager) Render(w http.ResponseWriter, template string, data in
 }
 
 func (t *TemplateManager) RenderTemplateErrors(w http.ResponseWriter, template string, data interface{}) {
-  if t.templatesParseError != nil {
-    panic(t.templatesParseError)
-  }
-
   if t.templatesReadError != nil {
     panic(t.templatesReadError)
+  }
+
+  if t.templatesParseError != nil {
+    panic(t.templatesParseError)
   }
 }
 
@@ -85,5 +85,5 @@ func initTemplateManager() {
 }
 
 func Render(w http.ResponseWriter, template string, data interface{}) {
-  templateManager.Render(w, template, data)
+  templateManager.renderFunc(w, template, data)
 }
